1e8c607300fb42ba519607e9ce41507a04de562f
David Fleissner <dffleiss@ncsu.edu>
2021-02-21 14:51:39 -0500

    Fixed light sensor block. Split up control and motor blocks. Some general reorganization.

--- updateApp.m
+++ updateApp.m
@@ -1,18 +1,4 @@
 function updateApp(varargin)
-    % No need to search for these every call
-    %persistent all_tag_objects
-    %persistent all_tags
-    %persistent last_call_time
-    %if isempty(all_tag_objects)
-    %    %get a handle to the GUI's 'current state' window
-    %    all_tag_objects = findall(0, '-property', 'tag');
-    %    all_tags = get(all_tag_objects, 'tag');
-    %    last_call_time = tic;
-    %end
-    
-    %disp(toc(last_call_time))
-    %last_call_time = tic;
-    
     all_tag_objects = findall(0, '-property', 'tag');
     all_tags = get(all_tag_objects, 'tag');
     % These correspond to the output names from Simulink
